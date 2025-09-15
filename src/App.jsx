a8e0e0a0d8e95f15e3819818d14aa2d04320ad24
ImLostPlsHelp <maurits1005@gmail.com>
2025-09-15 18:37:47 +0700

    Fixing indents and unused imports using eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import About from './components/About'
 import Footer from './components/Footer'
@@ -8,16 +5,16 @@ import Info from './components/Info'
 import Interests from './components/Interests'
 
 function App() {
-  return (
-    <>
-    <main>
-      <Info />
-      <About />
-      <Interests />
-      <Footer />
-    </main>
-    </>
-  )
+    return (
+        <>
+            <main>
+                <Info />
+                <About />
+                <Interests />
+                <Footer />
+            </main>
+        </>
+    )
 }
 
 export default App
